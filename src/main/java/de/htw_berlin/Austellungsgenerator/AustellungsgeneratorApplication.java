fe55e700a41c5e3d047cffe250bf013bf3e0256f
Nick <nickvl12.bvb@gmail.com>
2026-04-16 18:26:06 +0200

    Installed_Framework

---
+++ src/main/java/de/htw_berlin/Austellungsgenerator/AustellungsgeneratorApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.Austellungsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustellungsgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AustellungsgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw_berlin/Austellungsgenerator/AustellungsgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.Austellungsgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AustellungsgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
